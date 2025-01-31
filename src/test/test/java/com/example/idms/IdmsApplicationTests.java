f06d2877709a18968798d869b60ed268bec63e86
KumRaut <kumar.raut799@gmail.com>
2025-01-31 07:02:39 +0530

    Your commit message

---
+++ src/test/test/java/com/example/idms/IdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
